db8b6b5291381a4fb652ec2c352ea1dec4ea4666
Valentin Titov <Titov.V.Igo@sberbank.ru>
2019-08-07 18:27:15 +0300

    camel-test-jenkins w/ casc

--- camel-test-jenkins/src/test/java/io/jenkins/plugins/camel/kafka/JenkinsCamelKafkaTest.java
+++ camel-test-jenkins/src/test/java/io/jenkins/plugins/camel/kafka/JenkinsCamelKafkaTest.java
@@ -1,8 +1,12 @@
 package io.jenkins.plugins.camel.kafka;
 
 
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import jenkins.model.Jenkins;
 import lombok.extern.java.Log;
 import lombok.val;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.junit.rules.RuleChain;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -16,8 +20,22 @@ import static org.junit.Assert.assertTrue;
 @Log
 public class JenkinsCamelKafkaTest {
 
+    //@Rule
+    //public JenkinsRule jenkinsRule;
+    //@Rule
+    //public JenkinsRule jenkinsRule = new JenkinsRule();
+
+
     @Rule
-    public JenkinsRule jenkinsRule;
+    public RuleChain chain = RuleChain.outerRule( new EnvironmentVariables()
+        .set("CASC_JENKINS_CONFIG", getClass().getResource("JenkinsCamelKafkaTest.yml").toExternalForm()))
+        .around(new JenkinsConfiguredWithCodeRule());
+
+    @Test
+    public void loadFromCASC_JENKINS_CONFIG() {
+        Jenkins j = Jenkins.get();
+        assertEquals("configuration as code - JenkinsCamelKafkaTest", j.getSystemMessage());
+    }
 
     @Test
     public void kafkaTriggeredPipeline() throws Exception {
